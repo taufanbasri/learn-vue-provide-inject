bdb4190894b40a0df80beb87ec980d78ee4324a2
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2021-03-10 21:20:06 +0700

    Provide & Inject

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       :topic-title="activeTopic && activeTopic.title"
       :text="activeTopic && activeTopic.fullText"
     ></active-element>
-    <knowledge-base :topics="topics" @select-topic="activateTopic"></knowledge-base>
+    <knowledge-base @select-topic="activateTopic"></knowledge-base>
   </div>
 </template>
 
@@ -37,6 +37,11 @@ export default {
       this.activeTopic = this.topics.find((topic) => topic.id === topicId);
     },
   },
+  provide() {
+    return {
+      topics: this.topics
+    }
+  }
 };
 </script>
 
